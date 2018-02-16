cf02758b6e533598ac9e2d9cedce0ace5cf9f832
Jabrail Lezgintsev <lezgintsev13@yandex.ru>
2018-02-16 15:38:19 +0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from app import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', TemplateView.as_view(template_name='app/index.html'), name='index'),
     path('add/', views.CreatePlaylist.as_view(), name='create-playlist'),
     path('channel/<int:pk>/', views.ChannelUpdate.as_view(), name='channel'),
     path('channel/new/', views.ChannelCreate.as_view(), name='new-channel'),
