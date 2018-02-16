ce006f3af657b903216e14480c3f743a35623b66
Jabrail Lezgintsev <lezgintsev13@yandex.ru>
2018-02-16 14:40:49 +0300

    returned settings.py to default

--- playlist/settings.py
+++ playlist/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ds'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['m3u8.ru', ]
 
 # Application definition
 
@@ -147,6 +147,7 @@ except ImportError:
         from playlist.production import *
     except ImportError:
         pass
+
 from django.utils.translation import gettext_lazy as _
 
 LANGUAGES = [
